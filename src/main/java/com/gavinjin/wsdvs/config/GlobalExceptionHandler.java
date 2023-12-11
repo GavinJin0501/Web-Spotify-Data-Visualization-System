5f25588da2549389d40c72f9910e23f0dab1b8b7
JJY <jj2915@nyu.edu>
2023-12-11 15:55:07 -0500

    Fix exception handle logic

--- src/main/java/com/gavinjin/wsdvs/config/GlobalExceptionHandler.java
+++ src/main/java/com/gavinjin/wsdvs/config/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.gavinjin.wsdvs.config;
 
 import com.gavinjin.wsdvs.model.dto.BaseResponse;
 import com.gavinjin.wsdvs.utils.ResponseUtils;
+import com.gavinjin.wsdvs.utils.constant.StatusCode;
 import com.gavinjin.wsdvs.utils.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-    @ExceptionHandler(BusinessException.class)
-    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
-        log.error("BusinessException", e);
-        return ResponseUtils.error(e.getCode(), e.getMessage());
+    @ExceptionHandler({Exception.class})
+    public BaseResponse<?> businessExceptionHandler(Exception e) {
+        try {
+            BusinessException be = (BusinessException) e;
+            log.error("BusinessException", be);
+            return ResponseUtils.error(be.getCode(), be.getMessage());
+        } catch (Exception ee) {
+            log.error("Exception", e);
+            return ResponseUtils.error(StatusCode.SYSTEM_ERROR.getCode(), e.getMessage());
+        }
     }
 }
